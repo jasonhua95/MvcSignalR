0507d929569552c2d4a7a80e77adc527a95ee48a
华营杰 <jasonhua12@163.com>
2018-10-31 15:07:45 +0800

    代码初始化

---
+++ MvcSignalR/Scripts/qq.js
@@ -0,0 +1,55 @@
+$(function () {
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatServiceHub;
+    // 接受信息
+    chat.client.receiveMessage = function (message) {
+        receiveMessage(message);
+    };
+    $.connection.hub.start();
+
+    // 左边列表
+    $('.conLeft li').on('click', function () {
+        $(this).addClass('bg').siblings().removeClass('bg');
+        var intername = $(this).children('.liRight').children('.intername').text();
+        $('.headName').text(intername);
+        $('.newsList').html('');
+    });
+
+    //发送信息
+    $('.sendBtn').on('click', function () {
+        var news = $('#dope').val();
+        if (news == '') {
+            alert('不能为空');
+        } else {
+            chat.server.sendMessage(news);
+            sendMessage(news);
+            $('#dope').val('');
+        }
+    });
+
+    function sendMessage(message) {
+        var str = '';
+        str += '<li>' +
+                '<div class="nesHead"><img src="/Images/qq/1004.jpg"/></div>' +
+                '<div class="news"><img class="jiao" src="/Images/qq/20170926103645_03_02.jpg">' + message + '</div>' +
+            '</li>';
+        $('.newsList').append(str);
+        $('.conLeft').find('li.bg').children('.liRight').children('.infor').text(message);
+        $('.RightCont').scrollTop($('.RightCont')[0].scrollHeight);
+    }
+
+    function receiveMessage(message) {
+        var answer = '';
+        answer += '<li>' +
+					'<div class="answerHead"><img src="/Images/qq/1003.jpg"/></div>' +
+					'<div class="answers"><img class="jiao" src="/Images/qq/jiao.jpg">' + message + '</div>' +
+				'</li>';
+        $('.newsList').append(answer);
+        $('.RightCont').scrollTop($('.RightCont')[0].scrollHeight);
+    }
+});
+
+
+
+
+
---
+++ MvcSignalR/Services/ChatServiceHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MvcSignalR.Services
+{
+	/// <summary>
+	/// 数据交流类
+	/// </summary>
+	public class ChatServiceHub : Hub
+	{
+		//所有的ConnectionId
+		public static ConcurrentDictionary<string,DateTime> AllConnectionIDs = new ConcurrentDictionary<string, DateTime>();
+		//用户的ConnectionID，一个ID对应一个用户
+		public static ConcurrentDictionary<string, string> UserConnectionIDs = new ConcurrentDictionary<string, string>();
+
+		private Random rd = new Random();
+
+		public void Send(string message)
+		{
+			int i = 0;
+			while (i < 10000)
+			{
+				i++;
+				Thread.Sleep(100);
+				Clients.Client(Context.ConnectionId).broadcastMessage(Context.ConnectionId, message + rd.Next(1000));
+			}
+		}
+
+		public void SendMessage(string message)
+		{
+			Clients.Client(Context.ConnectionId).receiveMessage(message + rd.Next(1000));
+		}
+
+		public override Task OnConnected()
+		{
+			AllConnectionIDs.AddOrUpdate(Context.ConnectionId, DateTime.Now, (key, oldValue) => DateTime.Now);
+			return base.OnReconnected();
+		}
+	}
+}
